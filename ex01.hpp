6823fd89cea38a6e38579a8d7d8b7d73af66c0ea
moflotas <tima.sedov.2004@mail.ru>
2022-03-07 15:28:39 +0300

    ex01

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,10 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-void multiples (int& sum, int x, int n)
-{
+template<typename T>
 
+void multiples(T &sum, T x, int n) {
+    for (int i = 1; i <= n; i++) {
+        sum += i * x;
+    }
 }
